62c155b62b059287fa56f0ababd0f299a48a1539
saud <saudmalik619@gmail.com>
2025-08-06 17:06:21 +0500

    feat: centralize settings

--- settings.py
+++ settings.py
@@ -8,6 +8,17 @@ APP_ENV = os.getenv("APP_ENV", "production")
 HEADLESS = os.getenv("HEADLESS", "True").lower() == "true"
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS").strip('"')
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", 3306)
+DB_NAME = os.getenv("DB_NAME")
+
+
+ANTICAPTCHA_API_KEY = os.getenv("ANTICAPTCHA_API_KEY")
+
+
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", CELERY_BROKER_URL)
 
